e9ef8e71f2f349ee8e7629edcf22a915403d6554
SeonWoo Kim <ocean@SeonWooui-MacBookPro.local>
2022-02-20 17:17:42 +0900

    test

--- Component_test/src/component/btn.js
+++ Component_test/src/component/btn.js
@@ -5,7 +5,7 @@ const btn = () => {
     return (
         <Pressable style={{ backgroundColor: '#3498db', padding: 16, margin: 10, borderRadius: 8, }} onPress={() => alert('click!!')}>
             <Text style={{ color: 'white', fontSize: 24 }}>btn</Text>
-            
+            {/* testcommit */}
         </Pressable>
     )
 }
